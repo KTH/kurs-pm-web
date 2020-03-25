d9e6c8b9521ed7dcb418013574dcf8c351a125dd
Rickard Falk <rickard.falk@gmail.com>
2020-03-25 08:26:44 +0100

    Use Button instead of fake anchor element in menu

--- public/js/app/components/SideMenu.jsx
+++ public/js/app/components/SideMenu.jsx
@@ -1,36 +1,39 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react'
+
+import { Button } from 'reactstrap'
 
 const SideMenu = ({ courseCode = '', courseMemoItems = [] }) => {
   return (
     <div>
       <p>
-        &lsaquo; <a href="https://www.kth.se/student/kurser/kurser-inom-program">Kurs- och programkatalogen</a>
+        &lsaquo;&nbsp;
+        <a href="https://www.kth.se/student/kurser/kurser-inom-program">Kurs- och programkatalogen</a>
       </p>
       <p>
-        <b>Om kursen {courseCode}</b>
+        <b>
+          Om kursen&nbsp;
+          {courseCode}
+        </b>
       </p>
       <hr />
       <p>Inför kursval</p>
       <p>
         <b>Förbereda, gå (kurs-pm)</b>
       </p>
-      <ul>
+      <hr />
+      <p>
         {courseMemoItems.map(({ label, action, active }) => {
           return active ? (
-            <b>
-              <li key={label}>{label}</li>
-            </b>
+            <Button key={label} className="menu" color="link" disabled>
+              {label}
+            </Button>
           ) : (
-            <li key={label}>
-              <a onClick={() => action()}>{label}</a>
-            </li>
+            <Button key={label} className="menu" color="link" onClick={() => action()}>
+              {label}
+            </Button>
           )
         })}
-      </ul>
+      </p>
       <p>Slutföra ej avklarad kurs</p>
       <p>Kursens utveckling och historik</p>
     </div>
