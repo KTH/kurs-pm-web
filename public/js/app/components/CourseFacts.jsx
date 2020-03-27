db54ed767385d8e01ca932ef0f84c7952bac1bd8
Rickard Falk <rickard.falk@gmail.com>
2020-03-27 11:22:16 +0100

    Added link to school

--- public/js/app/components/CourseFacts.jsx
+++ public/js/app/components/CourseFacts.jsx
@@ -6,6 +6,8 @@ import i18n from '../../../../i18n'
 const englishTranslations = i18n.messages[0].messages
 const swedishTranslations = i18n.messages[1].messages
 
+const linkToSchool = (name = '') => `https://www.kth.se/${name.toLowerCase().split('/')[0]}`
+
 const version = (language, memoData) =>
   memoData.version ? (
     <div>
@@ -23,7 +25,9 @@ const offeredBy = (language, department) =>
   department.name ? (
     <div>
       <h3>{language === 'sv' ? swedishTranslations.offeredByTitle : englishTranslations.offeredByTitle}</h3>
-      <div dangerouslySetInnerHTML={{ __html: department.name }} />
+      <p>
+        <a href={linkToSchool(department.name)}>{department.name}</a>
+      </p>
     </div>
   ) : (
     <div>
