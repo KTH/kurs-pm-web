34b4167e97214740085bfa9973bb30ab713885e6
Rickard Falk <rickard.falk@gmail.com>
2020-04-01 12:34:23 +0200

    KIP-400
    Språkstyrning av länkar

--- public/js/app/pages/CourseMemo.jsx
+++ public/js/app/pages/CourseMemo.jsx
@@ -129,7 +129,7 @@ class CourseMemo extends Component {
               </Col>
               <Col lg="3">
                 <CourseFacts language={this.language} department={this.department} memoData={routerStore.memoData} />
-                <CourseLinks language={this.language} />
+                <CourseLinks language={routerStore.memoLanguage} />
                 <CourseContacts language={routerStore.memoLanguage} memoData={routerStore.memoData} />
               </Col>
             </Row>
