a4ff305a15e26cac4de3d707a886f10280ade601
Elena Rakhimova <elenara@kth.se>
2020-10-07 15:48:50 +0200

    fix: change header

--- public/js/app/components/CourseHeader.jsx
+++ public/js/app/components/CourseHeader.jsx
@@ -6,32 +6,26 @@ import { Row, Col } from 'reactstrap'
 const CourseHeader = ({ courseMemoName, courseTitle, courseCode, labels = {}, language }) => {
   const { adminLinkLabel } = labels
   return (
-    <>
-      <Row className="d-print-none">
-        <Col>
-          <h1 id="memo-title" className="course-header-title">
+    <Row>
+      <header className="pageTitle col">
+        <span role="heading" aria-level="1">
+          <span className="t1" id="memo-title">
             {courseMemoName}
-          </h1>
-        </Col>
-      </Row>
-      <Row className="pb-3 d-print-none">
-        <Col className="text-left" xs="12" lg="6">
-          <p id="memo-subtitle" className="secondTitle">
+          </span>
+          <span className="t4" id="memo-subtitle">
             {courseTitle}
-          </p>
-        </Col>
-        <Col className="text-lg-right" xs="12" lg="6">
-          <a
-            id="admin-link"
-            className="course-header-admin-link"
-            title={adminLinkLabel}
-            href={adminLink(courseCode, language)}
-          >
-            {adminLinkLabel}
-          </a>
-        </Col>
-      </Row>
-    </>
+          </span>
+        </span>
+        <a
+          title={adminLinkLabel}
+          className="right-link"
+          href={adminLink(courseCode, language)}
+          style={{ fontSize: '1rem' }}
+        >
+          {adminLinkLabel}
+        </a>
+      </header>
+    </Row>
   )
 }
 
