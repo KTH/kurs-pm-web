fa1999f854f8760acd757eb6abb275c0fac8a859
Rickard Falk <rickard.falk@gmail.com>
2020-11-20 15:09:53 +0100

    test: Added test for SideMenu component

--- public/js/app/components/__tests__/SideMenu.test.js
+++ public/js/app/components/__tests__/SideMenu.test.js
@@ -8,9 +8,46 @@ import i18n from '../../../../../i18n'
 
 const { sideMenuLabels } = i18n.messages[0]
 
+const mockCourseMemoItems = [
+  {
+    id: '1',
+    label: '1',
+    active: false,
+    url: 'https://test.com',
+    outdated: false
+  },
+  {
+    id: '2',
+    label: '2',
+    active: true,
+    url: 'https://test.com',
+    outdated: false
+  },
+  {
+    id: '3',
+    label: '3',
+    active: false,
+    url: 'https://test.com',
+    outdated: true
+  },
+  {
+    id: '4',
+    label: '4',
+    active: false,
+    url: 'https://test.com',
+    outdated: false
+  }
+]
+
 describe('Component <SideMenu>', () => {
   test('renders', (done) => {
     render(<SideMenu labels={sideMenuLabels} courseMemoItems={[]} />)
+    done()
+  })
+  test('renders memos', (done) => {
+    const { getAllByRole } = render(<SideMenu labels={sideMenuLabels} courseMemoItems={mockCourseMemoItems} />)
+    const listItems = getAllByRole('listitem')
+    expect(listItems).toHaveLength(10)
     done()
   })
 })
