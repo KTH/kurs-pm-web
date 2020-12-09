4bddf1e4b84f65a852e28710bd06b498b2112013
Rickard Falk <rickard.falk@gmail.com>
2020-12-09 22:22:19 +0100

    chore: Cleanup of integration test

--- test/mock-api/server.js
+++ test/mock-api/server.js
@@ -5,6 +5,8 @@ const config = require('./config')
 
 const app = express()
 config.paths.forEach((path) => {
+  // eslint-disable-next-line no-console
+  console.log('Added path', path.url)
   app.get(path.url, (req, res) => {
     res.send(path.response)
   })
