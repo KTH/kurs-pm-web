20ba38e3d577b31440bf643747d28f61d00507e0
Rickard Falk <rickard.falk@gmail.com>
2021-02-09 13:16:32 +0100

    fix: KUI-159 Presentation image margins and size
    Bilden justeras i förhållande till texten.

--- public/js/app/components/CoursePresentation.jsx
+++ public/js/app/components/CoursePresentation.jsx
@@ -7,15 +7,7 @@ const CoursePresentation = ({ courseImageUrl = '', introText = '', labels }) => 
       <h2 id="course-presentation" className="sr-only">
         {labels.coursePresentation}
       </h2>
-      <img
-        // >= lg : float left, x margins spacer * .5
-        // < lg : x margins auto, display block
-        className="float-lg-left mr-lg-4 mb-lg-3 mx-sm-auto d-sm-block"
-        height="auto"
-        width="150px"
-        src={courseImageUrl}
-        alt=""
-      />
+      <img id="course-presentation-image" className="float-sm-left" src={courseImageUrl} alt="" />
       <article dangerouslySetInnerHTML={{ __html: introText }} aria-labelledby="course-presentation" />
     </section>
   )
