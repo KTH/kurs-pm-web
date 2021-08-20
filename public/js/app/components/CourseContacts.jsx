a9b0c7a8ef4c6d0c4ab2932df7fb1bd9c78c5d3a
Elena Rakhimova <elenara@kth.se>
2021-08-20 16:00:33 +0200

    fix: fix visibility bug in contacts section

--- public/js/app/components/CourseContacts.jsx
+++ public/js/app/components/CourseContacts.jsx
@@ -70,20 +70,28 @@ const OtherContacts = ({ memoData, labels }) =>
     </>
   )
 
-const CourseContacts = ({ styleId = null, memoData = {}, labels = {} }) => (
-  <section id={styleId} aria-labelledby="memo-contacts">
-    <h2 id="memo-contacts" className="info">
-      {labels.courseContactsTitle}
-    </h2>
-    <div className="info-box text-break">
-      <CommunicationWithTeachers memoData={memoData} labels={labels} />
-      <CourseCoordinator memoData={memoData} labels={labels} />
-      <Teacher memoData={memoData} labels={labels} />
-      <TeacherAssistants memoData={memoData} labels={labels} />
-      <Examiner memoData={memoData} labels={labels} />
-      <OtherContacts memoData={memoData} labels={labels} />
-    </div>
-  </section>
-)
+const CourseContacts = ({ styleId = null, memoData = {}, labels = {} }) => {
+  const { visibleInMemo } = memoData
+  const {
+    communicationDuringCourse: isCommunicationWTVisible,
+    otherContacts: isOtherContactsVisible,
+    teacherAssistants: isTeacherAssistantsVisible,
+  } = visibleInMemo
+  return (
+    <section id={styleId} aria-labelledby="memo-contacts">
+      <h2 id="memo-contacts" className="info">
+        {labels.courseContactsTitle}
+      </h2>
+      <div className="info-box text-break">
+        {isCommunicationWTVisible && <CommunicationWithTeachers memoData={memoData} labels={labels} />}
+        <CourseCoordinator memoData={memoData} labels={labels} />
+        <Teacher memoData={memoData} labels={labels} />
+        {isTeacherAssistantsVisible && <TeacherAssistants memoData={memoData} labels={labels} />}
+        <Examiner memoData={memoData} labels={labels} />
+        {isOtherContactsVisible && <OtherContacts memoData={memoData} labels={labels} />}
+      </div>
+    </section>
+  )
+}
 
 export default CourseContacts
