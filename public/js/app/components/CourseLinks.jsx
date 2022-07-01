63f37dab2e2d3707f5522e2e8cf32093aed5b1c4
Elena Rakhimova <elenara@kth.se>
2022-07-01 14:23:21 +0200

    fix: typo for string formatting

--- public/js/app/components/CourseLinks.jsx
+++ public/js/app/components/CourseLinks.jsx
@@ -6,7 +6,7 @@ const CourseLinks = ({ language, labels }) => (
   <aside className="info-box text-break" aria-labelledby="student-links">
     <h2 id="student-links">{labels.linkHeaderTitle}</h2>
     {['rightsAndResponsibilities', 'courseAndExamination', 'administrateYouStudy'].map(linkTitle => (
-      <p key={`paragraph-for-link-{linkTitle}`}>
+      <p key={`paragraph-for-link-${linkTitle}`}>
         <a id={`link-${linkTitle}`} title={labels[linkTitle]} href={courseLinks(language)[linkTitle]}>
           {labels[linkTitle]}
         </a>
