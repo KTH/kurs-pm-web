f3cd225c5cee56c814fad0537112c12b4625117e
Benjamin Langlotz <benjala@kth.se>
2023-09-19 15:24:00 +0200

    fix(KUI-1102): extract mockAPIs

--- public/js/app/pages/__tests__/AboutCourseMemoEN.test.js
+++ public/js/app/pages/__tests__/AboutCourseMemoEN.test.js
@@ -4,76 +4,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { WebContextProvider } from '../../context/WebContext'
 
 import AboutCourseMemo from '../AboutCourseMemo'
-
-const mockMixKursPmDataApi = () => ({
-  20192: [
-    {
-      courseCode: 'KIP2720',
-      courseMemoFileName: 'memo-KIP272020192-14341833da79.pdf',
-      applicationCodes: ['2'],
-      semester: '20192',
-      isPdf: true,
-      state: 'FULL',
-    },
-    {
-      courseCode: 'KIP2720',
-      applicationCodes: ['1'],
-      semester: '20192',
-      memoEndPoint: 'KIP272020192-1',
-      memoCommonLangAbbr: 'en',
-      memoName: 'Autumn 2019-1 (Start date 26/08/2019, English)',
-      isPdf: false,
-      state: 'CANCELLED',
-    },
-  ],
-  20202: [
-    {
-      courseCode: 'KIP2720',
-      applicationCodes: ['1'],
-      semester: '20202',
-      memoEndPoint: 'KIP272020202-1',
-      memoCommonLangAbbr: 'en',
-      memoName: 'Autumn 2020-1 (Start date 24/08/2020, English)',
-      isPdf: false,
-      state: 'APPROVED',
-    },
-  ],
-})
-
-const mockMixKoppsApi = () => [
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20212',
-    state: 'CANCELLED',
-  },
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20202',
-    state: 'APPROVED',
-  },
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20192',
-    state: 'APPROVED',
-  },
-  {
-    shortName: '',
-    applicationCode: '2',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20192',
-    state: 'APPROVED',
-  },
-]
+import { mockMixKoppsApi, mockMixKursPmDataApi } from '../mockApis'
 
 const { getAllByRole, getAllByText, getByText } = screen
 jest.mock('react-router-dom', () => ({
--- public/js/app/pages/__tests__/AboutCourseMemoSV.test.js
+++ public/js/app/pages/__tests__/AboutCourseMemoSV.test.js
@@ -5,6 +5,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { WebContextProvider } from '../../context/WebContext'
 
 import AboutCourseMemo from '../AboutCourseMemo'
+import { mockMixKoppsApi, mockMixKursPmDataApi } from '../mockApis'
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -12,76 +13,6 @@ jest.mock('react-router-dom', () => ({
     pathname: 'localhost:3000/example/path',
   }),
 }))
-
-const mockMixKursPmDataApi = () => ({
-  20192: [
-    {
-      courseCode: 'KIP2720',
-      courseMemoFileName: 'memo-KIP272020192-14341833da79.pdf',
-      applicationCodes: ['2'],
-      semester: '20192',
-      isPdf: true,
-      state: 'APPROVED',
-    },
-    {
-      courseCode: 'KIP2720',
-      memoEndPoint: 'KIP272020192-1',
-      applicationCodes: ['1'],
-      semester: '20192',
-      memoCommonLangAbbr: 'en',
-      memoName: 'Autumn 2019-1 (Start date 26/08/2019, English)',
-      isPdf: false,
-      state: 'CANCELLED',
-    },
-  ],
-  20202: [
-    {
-      courseCode: 'KIP2720',
-      applicationCodes: ['1'],
-      semester: '20202',
-      memoEndPoint: 'KIP272020202-1',
-      memoCommonLangAbbr: 'en',
-      memoName: 'Autumn 2020-1 (Start date 24/08/2020, English)',
-      isPdf: false,
-      state: 'APPROVED',
-    },
-  ],
-})
-
-const mockMixKoppsApi = () => [
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20212',
-    state: 'CANCELLED',
-  },
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20202',
-    state: 'APPROVED',
-  },
-  {
-    shortName: '',
-    applicationCode: '1',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20192',
-    state: 'APPROVED',
-  },
-  {
-    shortName: '',
-    applicationCode: '2',
-    firstTuitionDate: '2023-01-17',
-    lastTuitionDate: '2023-03-17',
-    term: '20192',
-    state: 'APPROVED',
-  },
-]
 
 const { getAllByRole, getAllByText, getByText } = screen
 
